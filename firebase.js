f4fbfbd20c573922942e2248cc9311e55b6edc86
haotran1612-sudo <haotran1612@gmail.com>
2026-06-28 23:31:12 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,3 +1,21 @@
+import firebase from "https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js";
+import "https://www.gstatic.com/firebasejs/8.10.1/firebase-auth.js";
+import "https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "XXX",
+  authDomain: "XXX",
+  projectId: "XXX",
+  storageBucket: "XXX",
+  messagingSenderId: "XXX",
+  appId: "XXX"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const db = firebase.firestore();
+export const auth = firebase.auth();
+export const provider = new firebase.auth.GoogleAuthProvider();
 // firebase.js
 
 const firebaseConfig = {

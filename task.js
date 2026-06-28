dbd8568e076f626073b32aac5322abffa2ab184c
haotran1612-sudo <haotran1612@gmail.com>
2026-06-28 21:09:48 +0700

    Update task.js

--- task.js
+++ task.js
@@ -1,1 +1,51 @@
+// modules/task.js
 
+import { db } from "../firebase.js";
+
+export async function createTask(data){
+
+  const ref =
+    await db
+      .collection("tasks")
+      .add({
+        ...data,
+        createdAt:Date.now()
+      });
+
+  return ref.id;
+
+}
+
+export async function updateTask(id,data){
+
+  return db
+    .collection("tasks")
+    .doc(id)
+    .update(data);
+
+}
+
+export async function removeTask(id){
+
+  return db
+    .collection("tasks")
+    .doc(id)
+    .delete();
+
+}
+
+export async function getTasks(){
+
+  const snap =
+    await db
+      .collection("tasks")
+      .get();
+
+  return snap.docs.map(
+    x=>({
+      id:x.id,
+      ...x.data()
+    })
+  );
+
+}

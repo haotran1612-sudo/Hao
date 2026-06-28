e27b3ffe41138f098217886c06d85be7c64c51ac
haotran1612-sudo <haotran1612@gmail.com>
2026-06-28 22:48:08 +0700

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,52 +1,43 @@
-// ui/calendarView.js
+import { googleLogin, logout } from "./googleSync.js";
 
-export function renderWeek(events){
+export function showCalendar(){
+ document.getElementById("calendarPage").style.display="block";
+ document.getElementById("trackerPage").style.display="none";
+ document.getElementById("backupPage").style.display="none";
+}
 
- const root =
- document.querySelector("#calendar");
+export function showTracker(){
+ document.getElementById("trackerPage").style.display="block";
+ document.getElementById("calendarPage").style.display="none";
+ document.getElementById("backupPage").style.display="none";
+}
 
- root.innerHTML = "";
+export function showBackup(){
+ document.getElementById("backupPage").style.display="block";
+ document.getElementById("trackerPage").style.display="none";
+ document.getElementById("calendarPage").style.display="none";
+}
 
- events.forEach(event=>{
+export function showKanban(){
+ console.log("kanban");
+}
 
-   const div =
-   document.createElement("div");
-
-   div.className="event";
-
-   div.innerHTML=`
-     <div>${event.title}</div>
-   `;
-
-   root.append(div);
-
- });
-
+export function openTaskModal(){
+ document.getElementById(
+   "taskModal"
+ ).style.display="block";
 }
 
 export function initUI(){
-
  console.log("ui started");
-
 }
 
-// ===== PAGE =====
-
-export function showCalendar(){
-
- document.getElementById(
-   "calendarPage"
- ).style.display="block";
-
- document.getElementById(
-   "trackerPage"
- ).style.display="none";
-
- document.getElementById(
-   "backupPage"
- ).style.display="none";
-
-}
-
-// chỉ export cái đang tồn tại
+// expose ra HTML
 window.showCalendar = showCalendar;
+window.showTracker = showTracker;
+window.showBackup = showBackup;
+window.showKanban = showKanban;
+window.openTaskModal = openTaskModal;
+
+window.googleLogin = googleLogin;
+window.logout = logout;

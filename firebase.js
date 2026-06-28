6dd4d188ead53d2b9e5c21cbe50e650cc167e528
haotran1612-sudo <haotran1612@gmail.com>
2026-06-28 22:39:30 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,12 @@
 // firebase.js
 
 const firebaseConfig = {
-  apiKey:"",
-  authDomain:"",
-  projectId:""
+  apiKey: "AIzaSyDCI_kxfza5lY1tdFPsMSEQKC2xqFpbMpM",
+  authDomain: "whoami-73408.firebaseapp.com",
+  projectId: "whoami-73408",
+  storageBucket: "whoami-73408.firebasestorage.app",
+  messagingSenderId: "755566064562",
+  appId: "1:755566064562:web:15d0ec2626cf8aa4feeaab"
 };
 
 firebase.initializeApp(firebaseConfig);

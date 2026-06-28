270d021cd085834474bee90c8282350bb6539028
haotran1612-sudo <haotran1612@gmail.com>
2026-06-28 21:08:52 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,12 @@
+// firebase.js
 
+const firebaseConfig = {
+  apiKey:"",
+  authDomain:"",
+  projectId:""
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const db = firebase.firestore();
+export const auth = firebase.auth();

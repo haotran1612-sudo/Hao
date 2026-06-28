d49318aae5ab9b164e4531552a31efd61120c0c8
haotran1612-sudo <haotran1612@gmail.com>
2026-06-28 21:10:08 +0700

    Create calendar.js

---
+++ calendar.js
@@ -0,0 +1,49 @@
+// modules/calendar.js
+
+import { db } from "../firebase.js";
+
+export async function createEvent(event){
+
+ return db
+   .collection("calendarEvents")
+   .add({
+      ...event,
+      source:event.source||"calendar",
+      createdAt:Date.now()
+   });
+
+}
+
+export async function updateEvent(id,data){
+
+ return db
+   .collection("calendarEvents")
+   .doc(id)
+   .update(data);
+
+}
+
+export async function deleteEvent(id){
+
+ return db
+   .collection("calendarEvents")
+   .doc(id)
+   .delete();
+
+}
+
+export async function getEvents(){
+
+ const snap=
+ await db
+ .collection("calendarEvents")
+ .get();
+
+ return snap.docs.map(
+  x=>({
+    id:x.id,
+    ...x.data()
+  })
+ );
+
+}

d1b8883f30d993d35451d6597e913ea748fd2276
haotran1612-sudo <haotran1612@gmail.com>
2026-06-28 22:44:46 +0700

    Update ui.js

--- ui.js
+++ ui.js
@@ -2,45 +2,51 @@
 
 export function renderWeek(events){
 
- const root=
- document
- .querySelector(
- "#calendar"
- );
+ const root =
+ document.querySelector("#calendar");
 
- root.innerHTML="";
+ root.innerHTML = "";
 
- events.forEach(
- event=>{
+ events.forEach(event=>{
 
-const div=
-document
-.createElement(
-"div"
-);
+   const div =
+   document.createElement("div");
 
-div.className=
-"event";
+   div.className="event";
 
-div.innerHTML=
-`
-<div>
-${event.title}
-</div>
-`;
+   div.innerHTML=`
+     <div>${event.title}</div>
+   `;
 
-root.append(
-div
-);
+   root.append(div);
 
-});
+ });
 
 }
 
 export function initUI(){
 
- console.log(
-   "ui started"
- );
+ console.log("ui started");
 
 }
+
+// ===== PAGE =====
+
+export function showCalendar(){
+
+ document.getElementById(
+   "calendarPage"
+ ).style.display="block";
+
+ document.getElementById(
+   "trackerPage"
+ ).style.display="none";
+
+ document.getElementById(
+   "backupPage"
+ ).style.display="none";
+
+}
+
+// chỉ export cái đang tồn tại
+window.showCalendar = showCalendar;

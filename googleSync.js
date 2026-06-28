5a1a89056f601c5a44bb229ff5b5e2f40c44b51b
haotran1612-sudo <haotran1612@gmail.com>
2026-06-28 22:53:17 +0700

    Update googleSync.js

--- googleSync.js
+++ googleSync.js
@@ -1,58 +1,46 @@
-// modules/googleSync.js
+// googleSync.js
 
 export async function pushToGoogle(event){
 
- const token=
- localStorage
- .getItem(
-  "googleToken"
+ const token =
+ localStorage.getItem(
+   "googleToken"
  );
 
  if(!token)
- return;
+   return;
 
  await fetch(
- "https://www.googleapis.com/calendar/v3/calendars/primary/events",
- {
-   method:"POST",
-   headers:{
-      Authorization:
-      `Bearer ${token}`,
-      "Content-Type":
-      "application/json"
-   },
-   body:
-   JSON.stringify({
-
-     summary:
-     event.title,
-
-     start:{
-      dateTime:
-      event.start
+   "https://www.googleapis.com/calendar/v3/calendars/primary/events",
+   {
+     method:"POST",
+     headers:{
+       Authorization:`Bearer ${token}`,
+       "Content-Type":"application/json"
      },
+     body:JSON.stringify({
 
-     end:{
-      dateTime:
-      event.end
-     }
+       summary:event.title,
 
-   })
- });
+       start:{
+         dateTime:event.start
+       },
 
-}
+       end:{
+         dateTime:event.end
+       }
 
-
-export function initGoogleSync(){
-
- console.log(
-   "google sync started"
+     })
+   }
  );
 
 }
+
 export function googleLogin(){
 
- console.log("google login");
+ console.log(
+   "google login"
+ );
 
 }
 

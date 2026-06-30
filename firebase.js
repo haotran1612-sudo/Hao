6f357b138ad7a96b8f9de102dd2f9636d4fa3ed7
haotran1612-sudo <haotran1612@gmail.com>
2026-06-30 20:11:10 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,38 +1,59 @@
 // =======================
 // FIREBASE CONFIG
-// ======================= 
+// =======================
 
-// FIREBASE INIT
 const firebaseConfig = {
-  apiKey: "AIzaSyDCI_kxfza5lY1tdFPsMSEQKC2xqFpbMpM",
-  authDomain: "whoami-73408.firebaseapp.com",
-  projectId: "whoami-73408",
-  storageBucket: "whoami-73408.firebasestorage.app",
-  messagingSenderId: "755566064562",
-  appId: "1:755566064562:web:15d0ec2626cf8aa4feeaab"
+apiKey: "AIzaSyDCI_kxfza5lY1tdFPsMSEQKC2xqFpbMpM",
+
+authDomain:
+"whoami-73408.firebaseapp.com",
+
+projectId:
+"whoami-73408",
+
+storageBucket:
+"whoami-73408.firebasestorage.app",
+
+messagingSenderId:
+"755566064562",
+
+appId:
+"1:755566064562:web:15d0ec2626cf8aa4feeaab"
 };
 
-firebase.initializeApp(firebaseConfig);
-
-// EXPORT (QUAN TRỌNG)
-export const auth = firebase.auth();
-export const db = firebase.firestore();
-export const provider = new firebase.auth.GoogleAuthProvider();
 
 // =======================
-// EXPORT SERVICES
+// INIT FIREBASE
 // =======================
 
-export const auth = firebase.auth();
+if(!firebase.apps.length){
 
-export const db = firebase.firestore();
-
-export const provider =
-  new firebase.auth.GoogleAuthProvider();
-
-// Google Calendar scope
-provider.addScope(
-  "https://www.googleapis.com/auth/calendar"
+firebase.initializeApp(
+firebaseConfig
 );
 
-export default firebase;
+}
+
+
+// =======================
+// EXPORT
+// =======================
+
+export const auth =
+firebase.auth();
+
+export const db =
+firebase.firestore();
+
+export const provider =
+new firebase.auth
+.GoogleAuthProvider();
+
+
+// =======================
+// GOOGLE CALENDAR
+// =======================
+
+provider.addScope(
+"https://www.googleapis.com/auth/calendar"
+);
